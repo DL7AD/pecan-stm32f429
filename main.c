77a1b3f8efd236efa0204beabd14e04546c1a8dc
Sven Steudte <sven.steudte@gmail.com>
2016-01-30 02:17:02 +0100

    Implemented virtual timer for LED

--- main.c
+++ main.c
@@ -9,45 +9,64 @@
 #include "drivers/pi2c.h"
 #include "drivers/pac1720.h"
 
+static virtual_timer_t vt;
 uint32_t counter = 0;
 uint32_t error = 0;
 
+/**
+  * LED blinking routine
+  * RED LED blinks: One or more modules crashed (software watchdog)
+  * GREEN LED blinks: I'm alive! (STM32 crashed if not blinking)
+  * YELLOW LED: reserved
+  */
+static void led_cb(void *led_sw) {
+	// Switch LEDs
+	palWritePad(PORT(LED_GREEN), PIN(LED_GREEN), (bool)led_sw);	// Show I'M ALIVE
+	if(error)
+		palWritePad(PORT(LED_RED), PIN(LED_RED), (bool)led_sw);	// Show error
+
+	led_sw = !led_sw; // Set next state
+
+	chSysLockFromISR();
+	chVTSetI(&vt, MS2ST(500), led_cb, led_sw);
+	chSysUnlockFromISR();
+}
+
+/**
+  * Main routine is starting up system, runs the software watchdog (module monitoring), controls LEDs
+  */
 int main(void) {
-	halInit();			// Startup HAL
-	chSysInit();		// Startup RTOS
+	halInit();					// Startup HAL
+	chSysInit();				// Startup RTOS
 
-	DEBUG_INIT();		// Debug Init (Serial debug port, LEDs)
-
+	DEBUG_INIT();				// Debug Init (Serial debug port, LEDs)
 	TRACE_INFO("MAIN > Startup");
+
 	TRACE_INFO("MAIN > Startup SENSOR I2C");
 	i2cInit();					// Startup I2C
-	initEssentialModules();		// Startup required modules
-	initModules();				// Startup optional modules
+	initEssentialModules();		// Startup required modules (input/output modules)
+	initModules();				// Startup optional modules (eg. POSITION, LOG, ...)
 	pac1720_init();				// Startup current measurement
 
+	// Initialize LED timer
+	chVTObjectInit(&vt);
+	chVTSet(&vt, MS2ST(500), led_cb, 0);
+
 	while(true) {
-		palWritePad(PORT(LED_GREEN), PIN(LED_GREEN), counter%2);	// Show I'M ALIVE
-		if(error)
-			palWritePad(PORT(LED_RED), PIN(LED_RED), counter%2);	// Show error
+		// Print time
+		PRINT_TIME("MAIN");
 
-		if(counter%60 == 0) {
-
-			// Print time
-			PRINT_TIME("MAIN");
-
-			// Module integration check (software watchdog)
-			for(uint8_t i=0; i<moduleCount; i++) {
-				if(ST2S(chVTGetSystemTimeX()-modules[i]->lastCycle) <= (uint32_t)modules[0]->cycle) {
-					TRACE_INFO("MAIN > Module %s OK", modules[i]->name);
-				} else {
-					TRACE_ERROR("MAIN > Module %s CRASHED", modules[i]->name);
-					error = 1;
-				}
+		// Software watchdog
+		for(uint8_t i=0; i<moduleCount; i++) {
+			if(ST2S(chVTGetSystemTimeX()-modules[i]->lastCycle) <= (uint32_t)modules[0]->cycle) {
+				TRACE_INFO("MAIN > Module %s OK", modules[i]->name);
+			} else {
+				TRACE_ERROR("MAIN > Module %s CRASHED", modules[i]->name);
+				error = 1; // Lets red LED blink in virtual timer
 			}
-
 		}
 
-		chThdSleepMilliseconds(500);
+		chThdSleepMilliseconds(30000);
 		counter++;
 	}
 }
